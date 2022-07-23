d65b4717238b3a8bdf274249cf09435abedcf5e9
Harrison Hough <harrisonhough4@gmail.com>
2022-07-23 17:25:41 +0300

    Setup of ball toss project (#2)
    * added local gitignore
    
    * project setup
    
    * run rider reformat and cleanup
    
    * cleanup and fixed all rider warnings

---
+++ BallTossGame/Assets/Scripts/BallRecycle.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using UnityEngine;
+
+/*
+* AUTHOR: Harrison Hough   
+* COPYRIGHT: Harrison Hough 2021
+* VERSION: 1.0
+* SCRIPT: Ball Recycle Class 
+*/
+
+public class BallRecycle : MonoBehaviour
+{
+    public float recycleTime = 3f;
+    private Ball ball;
+
+    private void Awake()
+    {
+        ball = GetComponent<Ball>();
+    }
+
+    public void StartRecycle()
+    {
+        StartCoroutine(RecycleTimer());
+    }
+
+    IEnumerator RecycleTimer()
+    {
+        yield return new WaitForSeconds(recycleTime);
+        Recycle();
+    }
+
+    private void Recycle()
+    {
+        ball.ResetPosition();
+    }
+}
