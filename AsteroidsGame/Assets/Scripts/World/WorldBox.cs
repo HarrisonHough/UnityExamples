3f24a46a4cff16a1a992ee769bc1a401445b96b9
Harrison Hough <harrisonhough4@gmail.com>
2022-07-23 16:24:02 +0300

    Import and setup Asteroids base project (#1)
    * add local gitignore
    
    * added Asteroids project
    
    * run rider reformat and code cleanup
    
    * pass of scripts to fix rider warnings

---
+++ AsteroidsGame/Assets/Scripts/World/WorldBox.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+
+/*
+* AUTHOR: Harrison Hough   
+* COPYRIGHT: Harrison Hough 2018
+* VERSION: 1.0
+* SCRIPT: World Box Class
+*/
+
+/// <summary>
+/// 
+/// </summary>
+public class WorldBox : MonoBehaviour
+{
+
+    /// <summary>
+    /// 
+    /// </summary>
+    private void Start()
+    {
+        SetToScreenSize();
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    void SetToScreenSize()
+    {
+        Vector3 position = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width / 2, Screen.height / 2, 7));
+        position.y = 0;
+        transform.position = position;
+        
+        float maxPositionX = Mathf.Abs(Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 8)).x);
+        float maxPositionZ = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, 0, 8)).z;
+        transform.localScale = new Vector3(maxPositionX * 2, 1, Mathf.Abs(maxPositionZ * 2));
+    }
+
+
+}
---
+++ AsteroidsGame/README.md
@@ -0,0 +1,13 @@
+![Featured Screenshot: Asteroids](http://harrisonhough.com/wp-content/uploads/2017/05/Asteroids.png)
+
+# Ateroids Game
+A simple 3D remake of the old Asteroids game made in Unity
+
+## Description
+A 3D remake of the old Asteroids style space shooter. I created this in just a few days, I created all scripts and art assets myself. Also created some of the shaders.
+
+## Design Patterns Used
+* Singleton
+* Generic Classes
+* Object Pooling
+* Interfaces
